33ca02e630e6efc1ad91280f2178eb809fb3a74d
BeatusL <vagner.aa@edu.spbstu.ru>
2024-03-15 18:04:16 +0300

    /

--- pythonProject/CM3/lab3.py
+++ pythonProject/CM3/lab3.py
@@ -10,21 +10,24 @@ h_print = 0.1
 intervalStart = 1
 intervalEnd = 2
 
+
 def preciseY(t):
     return t * t * t
 
 
-def rk45(f, T, Y0):
-    r = integrate.ode(f).set_integrator(integratorName, atol = eps)
-    r.set_initial_value(Y0, T[0])
+def rk45(f, T, X0):
+    r = integrate.ode(f).set_integrator(integratorName, atol=eps).set_initial_value(X0, T[0])
 
-    Y = np.zeros((len(T), len(Y0)))
-    Y[0] = Y0
+    X = np.zeros((len(T), len(X0)))
+    X[0] = X0
 
     for i in range(1, len(T)):
-        Y[i] = r.integrate(T[i])
+        X[i] = r.integrate(T[i])
 
-    return Y
+        if not r.successful():
+            raise RuntimeError('Integration unsuccessful')
+
+    return X
 
 
 def backward_euler(f, T, Y0):
@@ -35,7 +38,8 @@ def backward_euler(f, T, Y0):
 
         def equations(X):
             return [
-                Y[i - 1][j] + (T[i] - T[i - 1]) * f1(X)[j] - X[j] for j in range(len(X))
+                #Y[i - 1][j] + (T[i] - T[i - 1]) * f1(X)[j] - X[j] for j in range(len(X))
+                Y[i - 1][j] + (T[i] - T[i - 1]) * f1(X-1)[j] - X[j] for j in range(len(X))
             ]
 
         root = fsolve(equations, [0] * len(Y0), xtol=1e-14, maxfev=2 ** 30)
@@ -45,20 +49,20 @@ def backward_euler(f, T, Y0):
     return Y
 
 
-def f(t, Y):
-    dY = np.zeros(len(Y))
-    dY[0] = Y[1]
-    dY[1] = (2 * t * Y[1] - 2 * Y[0]) / (t ** 2 - 1)
-    return dY
+def f(t, X):
+    dX = np.zeros(len(X))
+    dX[0] = X[1]
+    dX[1] = (6 * X[0]) / (t ** 2)
+    return dX
 
 
 for h in 0.1, 0.05, 0.025, 0.0125:
     print(f"{h = }:")
-    rng = np.arange(2, 3 + h - 1e-9, h)
+    rng = np.arange(1, 2 + h - 1e-9, h)
     n_steps = len(rng)
-    Y0 = [5, 4]
-    res_rk45 = [i[0] for i in rk45(f, rng, Y0)]
-    res_be = [i[0] for i in backward_euler(f, rng, Y0)]
+    X0 = [1, 3]
+    res_rk45 = [i[0] for i in rk45(f, rng, X0)]
+    res_be = [i[0] for i in backward_euler(f, rng, X0)]
     res_precise = [preciseY(rng[i]) for i in range(n_steps)]
     if h == 0.1:
         print("t\t\t rk45\t\t\t backward euler\t\t precise")
