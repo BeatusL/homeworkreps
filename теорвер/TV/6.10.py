42835fb497f547ad5034ced3c026137727dcab29
BeatusL <vagner.aa@edu.spbstu.ru>
2024-03-28 15:09:58 +0300

    .

--- теорвер/TV/6.10.py
+++ теорвер/TV/6.10.py
@@ -7,15 +7,8 @@ for i in range(tr):
     if i / tr * 100 % 1 == 0: print(i / tr * 100)
     cPos = randint(0, 100000)
     hPos = randint(0, 100000)
-    if (20000 <= cPos <= 40000 or 65000 <= cPos <= 90000) and (abs(cPos - hPos) <= 10000):
+    if (20000 <= cPos <= 40000 or 65000 <= cPos <= 90000) and (abs(cPos - hPos) <= 5000):
         pos += 1
-
-    c = randint(0, 100)
-    d = randint(0, 100)
-
-    if 20 <= d <= 40 or 65 <= d <= 90:
-        if abs(c - d) <= 10:
-            pos += 1
 
 print("--------------")
 print(pos / tr)
