7692ccf5d7bb8855fef3232ac3068ce210e62234
BeatusL <vagner.aa@edu.spbstu.ru>
2024-04-02 09:05:22 +0300

    .

---
+++ DC2/2.py
@@ -0,0 +1,13 @@
+def e1(name):
+    print("Hello, ", name, "!", sep="")
+
+def e2(price):
+    print(price * 1.1)
+
+def e3(p, q):
+    print(p * q)
+
+def e4(income):
+    return (income / 4 * 12)
+
+def e5()
